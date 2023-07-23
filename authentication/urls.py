bc7ad4428cf52f6ccbcafe7aacf7dcdd00e3f40d
Nicho <mac@Nicho.local>
2023-07-23 12:51:54 +0300

    refactor the code

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,10 +5,6 @@ from dj_rest_auth.views import LoginView, LogoutView
 from .views import *
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('api-keys/', APIKeysView.as_view(), name='api-keys'),
-    path('validate-api-keys/', ValidateAPIKeysView.as_view(),
-         name='validate-api-keys')
+   
 
 ]
